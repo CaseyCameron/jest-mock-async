a05c18e1deaa2f4d94207b8f116e498c50fc88d8
CaseyCameron <mcasey.cameron@gmail.com>
2022-07-11 11:01:04 -0700

    mocked axios call

--- swapiGetter.test.js
+++ swapiGetter.test.js
@@ -1,9 +1,16 @@
 import swapiGetter from './swapiGetter';
+import mockAxios from 'axios';
+
+jest.mock('axios');
+mockAxios.get.mockImplementation(
+  ()=> Promise.resolve({ data: { name: 'Luke Skywalker' } })
+)
 
 describe('swapiGetter', () => {
   test('should return a name', async () => {
     const result = await swapiGetter(1);
     console.log('result', result)
     expect(result).toBe('Luke Skywalker');
+    expect(mockAxios.get).toHaveBeenCalledTimes(1);
   });
 });
